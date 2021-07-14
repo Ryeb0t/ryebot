87274567ea7f4ddc94d69d969ed7316cf31aa787
Ryeb0t <87270706+Ryeb0t@users.noreply.github.com>
2021-07-15 00:39:02 +0200

    Add path to virtual environment to global list of paths

--- ryebot/bot/__init__.py
+++ ryebot/bot/__init__.py
@@ -1,6 +1,9 @@
 import os.path
 
 PATHS = { 'localdata':'', 'wikis':'', 'global_config':''}
+
 PATHS['localdata'] = os.path.join(os.path.expanduser('~'), 'ryebot', 'localdata')
+
+PATHS['venv'] = os.path.join(PATHS['localdata'], '.ryebotvenv')
 PATHS['wikis'] = os.path.join(PATHS['localdata'], 'wikis')
 PATHS['global_config'] = os.path.join(PATHS['localdata'], 'global_config')

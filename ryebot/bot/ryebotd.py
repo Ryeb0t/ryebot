476e0391dd648e470a885eff4f01dbfa3bd4efe7
Ryeb0t <87270706+Ryeb0t@users.noreply.github.com>
2021-07-15 00:42:43 +0200

    Create ryebotd.py

---
+++ ryebot/bot/ryebotd.py
@@ -0,0 +1,26 @@
+import logging
+import os
+import time
+
+from daemon import DaemonContext
+
+from ryebot.bot import PATHS
+
+
+LOGFILE = '.log'
+
+
+def maind():
+    logging.basicConfig(level=logging.INFO, filename=os.path.join(PATHS['localdata'], LOGFILE), format='[%(asctime)s] %(message)s', datefmt='%a %b %d %H:%M:%S %Y')
+    while True:
+        time.sleep(6)
+        logging.info('heartbeat from daemon')
+
+
+def main():
+    with DaemonContext():
+        maind()
+
+
+if __name__ == '__main__':
+    main()

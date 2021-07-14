e9d84f0ec7ecb606ee6131056e6f003e48c710ef
Ryeb0t <87270706+Ryeb0t@users.noreply.github.com>
2021-07-15 01:20:17 +0200

    Add base and package paths to list of global paths

--- ryebot/bot/__init__.py
+++ ryebot/bot/__init__.py
@@ -1,8 +1,12 @@
 import os.path
 
-PATHS = { 'localdata':'', 'wikis':'', 'global_config':''}
+PATHS = {}
 
-PATHS['localdata'] = os.path.join(os.path.expanduser('~'), 'ryebot', 'localdata')
+PATHS['base'] = os.path.join(os.path.expanduser('~'), 'ryebot')
+
+PATHS['package'] = os.path.join(PATHS['base'], 'ryebot')
+
+PATHS['localdata'] = os.path.join(PATHS['base'], 'localdata')
 
 PATHS['venv'] = os.path.join(PATHS['localdata'], '.ryebotvenv')
 PATHS['wikis'] = os.path.join(PATHS['localdata'], 'wikis')

e9ae820d5f0437222ff837bcddfd95cd925a7c6f
Ryeb0t <87270706+Ryeb0t@users.noreply.github.com>
2021-07-15 02:19:50 +0200

    Log all changes to the local data directory via daemon

--- ryebot/bot/ryebotd.py
+++ ryebot/bot/ryebotd.py
@@ -3,6 +3,8 @@ import os
 import time
 
 from daemon import DaemonContext
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
 
 from ryebot.bot import PATHS
 
@@ -10,8 +12,19 @@ from ryebot.bot import PATHS
 LOGFILE = '.log'
 
 
+def start_monitoring():
+    monitored_directory = PATHS['localdata']
+    observer = Observer()
+    observer.schedule(LoggingEventHandler(), monitored_directory, recursive=True)
+    observer.start()
+
+
 def maind():
-    logging.basicConfig(level=logging.INFO, filename=os.path.join(PATHS['localdata'], LOGFILE), format='[%(asctime)s] [pid %(pid)d] %(message)s', datefmt='%a %b %d %H:%M:%S %Y')
+    logging.basicConfig(level=logging.INFO, filename=os.path.join(PATHS['localdata'], LOGFILE),
+        format='[%(asctime)s] [pid %(pid)d] %(message)s', datefmt='%a %b %d %H:%M:%S %Y')
+
+    start_monitoring()
+
     while True:
         time.sleep(6)
         logging.info('heartbeat from daemon', extra={'pid': os.getpid()})

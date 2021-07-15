841a76d12d06978c07d5fdb16e4e6df5cd498e0e
Ryeb0t <87270706+Ryeb0t@users.noreply.github.com>
2021-07-15 02:02:00 +0200

    Find procs by content of each cmdline element

--- ryebot/custom_utils/ps_util.py
+++ ryebot/custom_utils/ps_util.py
@@ -1,10 +1,11 @@
 import psutil
 
 
-def find_procs_by_name(name):
-    """Return a list of processes whose `cmdline()` list contains `name`."""
+def find_procs_by_cmd(cmd):
+    """Return a list of processes where any of the `cmdline()` list elements contain `cmd`."""
     ls = []
     for p in psutil.process_iter(['cmdline']):
-        if name in p.info['cmdline']:
-            ls.append(p)
+        for c in p.info['cmdline']:
+            if cmd in c:
+                ls.append(p)
     return ls

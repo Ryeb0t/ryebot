8e7a11866b375c454bc42794d48784032be88bef
Ryeb0t <87270706+Ryeb0t@users.noreply.github.com>
2021-07-17 01:27:44 +0200

    Add script for actual, functional login

--- ryebot/bot/daemon_handlers.py
+++ ryebot/bot/daemon_handlers.py
@@ -1,8 +1,10 @@
 import logging
 import os
+import psutil
 
 from ryebot.bot import PATHS
 from ryebot.bot.wiki_manager import ONLINESTATUSFILENAME
+from ryebot.bot.status_displayer import OnlineStatus
 
 
 class FileModifiedEventHandler():
@@ -27,6 +29,18 @@ class FileModifiedEventHandler():
             return
         if os.path.dirname(os.path.dirname(self.file_path)) != PATHS['wikis']:
             return
+
         wikiname = os.path.basename(os.path.dirname(self.file_path))
-        newstatus = 'online' if os.stat(self.file_path).st_size > 0 else 'offline'
-        self.logger.info(f'Going {newstatus} on the "{wikiname}" wiki.', extra={'pid': os.getpid()})
+        if os.stat(self.file_path).st_size > 0:
+            newstatus = OnlineStatus.ONLINE
+        else:
+            newstatus = OnlineStatus.OFFLINE
+
+        self.logger.info(f'Going {str(newstatus).lower()} on the "{wikiname}" wiki.')
+
+        if newstatus == OnlineStatus.ONLINE:
+            python_command = os.path.join(PATHS['venv'], 'bin', 'python3')
+            script_file = os.path.join(PATHS['package'], 'bot', 'ryebotscript.py')
+            wikidirectory = os.path.join(PATHS['wikis'], wikiname)
+            psutil.Popen([python_command, script_file, '_login'], cwd=wikidirectory)
+            self.logger.info('Started new Python process for logging in.')
---
+++ ryebot/bot/login_and_logout.py
@@ -0,0 +1,51 @@
+import logging
+import os
+from pathlib import Path
+import time
+
+from ryebot.bot import PATHS
+from ryebot.custom_utils.wiki_util import login_to_wiki as login
+from ryebot.bot.status_displayer import LoginStatus
+from ryebot.bot.wiki_manager import LOGINSTATUSFILENAME
+
+
+def login_to_wiki(wikiname: str):
+    _modify_loginstatus_file(wikiname, newstatus=LoginStatus.LOGGING_IN)
+
+    try:
+        site, login_log = login(wikiname, return_log=True)
+    except Exception as e:
+        _modify_loginstatus_file(wikiname, newstatus=LoginStatus.LOGGED_OUT)
+        logging.info(f"Error while logging in to the \"{wikiname}\" wiki: " + str(e))
+        return
+
+    logging.info(login_log)
+    _modify_loginstatus_file(wikiname, newstatus=LoginStatus.LOGGED_IN, newlastlogin=time.time())
+
+
+def _modify_loginstatus_file(wiki: str, newstatus: LoginStatus=None, newlastlogin: float=None, newlastlogout: float=None):
+    loginstatusfile = os.path.join(PATHS['wikis'], wiki, LOGINSTATUSFILENAME)
+    if not os.path.exists(loginstatusfile):
+        Path(loginstatusfile).touch() # create the file
+
+    lines = []
+
+    # read current lines in the file
+    with open(loginstatusfile) as f:
+        lines = f.readlines()
+
+    for _ in range(3-len(lines)):
+        # if the file has fewer than three lines, then append the missing number of lines
+        lines.append('\n')
+
+    # modify the lines
+    if newstatus:
+        lines[0] = str(newstatus) + '\n'
+    if newlastlogin:
+        lines[1] = str(newlastlogin) + '\n'
+    if newlastlogout:
+        lines[2] = str(newlastlogout)
+
+    # write modified lines to file
+    with open(loginstatusfile, 'w+') as f:
+        f.writelines(lines)

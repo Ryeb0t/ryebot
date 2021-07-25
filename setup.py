941d276d8e06733806feb377c81616416633a5ad
Ryeb0t <87270706+Ryeb0t@users.noreply.github.com>
2021-07-25 19:59:35 +0200

    Add more versions to required packages in setup.py

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ LICENSE = 'MIT'
 URL = 'https://github.com/Ryeb0t/ryebot'
 PYTHON_VERSION = '>=3.8.0'
 REQUIREMENTS = [
-    'mwclient',
-    'mwparserfromhell',
+    'mwclient>=0.10.1',
+    'mwparserfromhell>=0.6.2',
     'click>=8.0.0',
-    'psutil',
-    'watchdog',
-    'python-daemon',
-    'pid',
+    'psutil>=5.8.0',
+    'watchdog>=2.1.0',
+    'python-daemon>=2.3.0',
+    'pid>=3.0.4',
     'beautifultable>=1.0.0'
 ]
 # command that will be used to execute the program from the command line

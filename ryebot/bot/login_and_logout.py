11ed37f7ac1174ee6af39322874cd9b8988e99f1
Ryeb0t <87270706+Ryeb0t@users.noreply.github.com>
2021-08-29 23:25:03 +0200

    Add class to handle the login status file

--- ryebot/bot/login_and_logout.py
+++ ryebot/bot/login_and_logout.py
@@ -76,6 +76,140 @@ class LoginControl():
                 f.write('11')
 
 
+class LoginStatus():
+    """Class for reading and modifying the login status file of a wiki."""
+
+    def __init__(self, wiki: str='', file: str=''):
+        """Access the login status file of a wiki, either using the wiki name or the file name directly."""
+
+        if file:
+            self.statusfile = file
+        elif wiki:
+            self.statusfile = os.path.join(PATHS['wikis'], *get_wiki_directory_from_name(wiki), LOGINSTATUSFILE)
+        else:
+            raise ValueError('LoginStatus requires either a wiki name or a file name!')
+
+        if not os.path.exists(self.statusfile):
+            Path(self.statusfile).touch() # create the file
+
+        # read current lines in the file
+        with open(self.statusfile) as f:
+            lines = f.readlines()
+
+        for _ in range(3-len(lines)):
+            # if the file has fewer than three lines, then append the missing number of lines
+            lines.append('\n')
+
+
+    def _modify_line(self, linenumber: int, new_linecontent: str):
+        """Change the content of a line in the status file."""
+
+        # read current lines in the file
+        with open(self.statusfile) as f:
+            lines = f.readlines()
+
+        # modify the relevant line
+        lines[linenumber] = new_linecontent
+
+        # write modified lines to file
+        with open(self.statusfile, 'w+') as f:
+            f.writelines(lines)
+
+
+    @property
+    def status(self):
+        """Return the current login status, as an `ELoginStatus` value."""
+
+        with open(self.statusfile) as f:
+            try:
+                line = f.readline().strip() # first line in the file
+                return ELoginStatus(int(line))
+            except (IndexError, ValueError):
+                # reading the first line might have failed,
+                # or conversion to int/LoginStatus might have failed,
+                # so consider the status to be unknown
+                return ELoginStatus.UNKNOWN
+
+    @status.setter
+    def status(self, value: ELoginStatus):
+        """Set the "status" part of the login status file."""
+
+        self._modify_line(0, str(value.value) + '\n')
+
+
+    @property
+    def last_login(self):
+        """Return the time of last login, as a `struct_time` value."""
+
+        with open(self.statusfile) as f:
+            try:
+                f.readline()
+                line = f.readline().strip() # second line in the file
+                return time.gmtime(float(line))
+            except ValueError:
+                # reading the second line might have failed,
+                # or conversion to float might have failed,
+                # so return the default Jan 1, 1970
+                return time.gmtime(0)
+
+    @last_login.setter
+    def last_login(self, value: float):
+        """Set the "last_login" part of the login status file."""
+
+        self._modify_line(1, str(value) + '\n')
+
+
+    @property
+    def last_logout(self):
+        """Return the time of last logout, as a `struct_time` value."""
+
+        with open(self.statusfile) as f:
+            try:
+                f.readline()
+                f.readline()
+                line = f.readline().strip() # third line in the file
+                return time.gmtime(float(line))
+            except ValueError:
+                # reading the third line might have failed,
+                # or conversion to float might have failed,
+                # so return the default Jan 1, 1970
+                return time.gmtime(0)
+
+    @last_logout.setter
+    def last_logout(self, value: float):
+        """Set the "last_logout" part of the login status file."""
+
+        self._modify_line(2, str(value))
+
+
+    @property
+    def all_info(self):
+        """Return a tuple of the current status and the time of last login and logout."""
+
+        all_info = []
+
+        with open(self.statusfile) as f:
+            status = f.readline().strip()
+            last_login = f.readline().strip()
+            last_logout = f.readline().strip()
+
+        try:
+            all_info.append(ELoginStatus(int(status)))
+        except ValueError:
+            all_info.append(ELoginStatus.UNKNOWN)
+        try:
+            all_info.append(time.gmtime(float(last_login)))
+        except ValueError:
+            all_info.append(time.gmtime(0))
+        try:
+            all_info.append(time.gmtime(float(last_logout)))
+        except ValueError:
+            all_info.append(time.gmtime(0))
+
+        return tuple(all_info)
+
+
+
 def login_to_wiki(wikiname: str, logger: logging.Logger):
     _modify_loginstatus_file(logger, wikiname, newstatus=ELoginStatus.LOGGING_IN)
 

542060e461bc59f9e5f7997b7a6ab504dd3ee303
Ryeb0t <87270706+Ryeb0t@users.noreply.github.com>
2021-08-29 23:03:34 +0200

    Use setter method

--- ryebot/bot/login_and_logout.py
+++ ryebot/bot/login_and_logout.py
@@ -47,38 +47,33 @@ class LoginControl():
             self.controlfile = os.path.join(PATHS['wikis'], *get_wiki_directory_from_name(wiki), LOGINCONTROLFILE)
         else:
             raise ValueError('LoginControl requires either a wiki name or a file name!')
-        
+
         if not os.path.exists(self.controlfile):
             Path(self.controlfile).touch() # create the file
 
 
-    def command_login(self):
-        """Set the content of the wiki's login control file size to 1, indicating that the bot should login there."""
-
-        with open(self.controlfile, 'w') as f:
-            f.write('1')
-
-
-    def command_logout(self):
-        """Set the content of the wiki's login control file size to 2, indicating that the bot should logout there."""
-
-        with open(self.controlfile, 'w') as f:
-            f.write('11')
-
-
     @property
     def command(self):
-        """Return the content of the login control file, as a `LoginControlCommand` value."""
-        
+        """Return the content of the login control file, as an `ELoginControlCommand` value."""
+
         # parse the size of the file
         filesize = os.stat(self.controlfile).st_size
         try:
             return ELoginControlCommand(int(filesize))
         except ValueError:
             # the file size is not in the enum's values,
-            # so consider the control command to be "do nothing"
-            return ELoginControlCommand.DO_NOTHING
+            # so consider the control command to be unknown
+            return ELoginControlCommand.UNKNOWN
 
+    @command.setter
+    def command(self, value: ELoginControlCommand):
+        """Set the content of the wiki's login control file, indicating that the bot should login/logout there."""
+
+        with open(self.controlfile, 'w') as f:
+            if value == ELoginControlCommand.DO_LOGIN:
+                f.write('1')
+            elif value == ELoginControlCommand.DO_LOGOUT:
+                f.write('11')
 
 
 def login_to_wiki(wikiname: str, logger: logging.Logger):

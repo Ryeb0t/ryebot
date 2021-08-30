243d1574105a2e33bbda6678f38691b418fe9618
Ryeb0t <87270706+Ryeb0t@users.noreply.github.com>
2021-08-30 14:49:49 +0200

    Remove reference to the ryebot module from outside utility function

--- ryebot/custom_mwclient/auth_credentials.py
+++ ryebot/custom_mwclient/auth_credentials.py
@@ -2,29 +2,21 @@ import json
 import os
 
 from .errors import InvalidUserFile
-from ryebot.bot import PATHS
 
 
 class AuthCredentials(object):
-    file_pattern = 'wiki_account_{}.json'
     username = None
     password = None
 
-    def __init__(self, user_name):
-        """
-        Stores username and password for future use with a WikiClient.
-        Username and password are taken from a JSON file in the global config
-        directory that should have a "username" key and a "password" key.
-        :param user_name: Name of the user for which a JSON file with credentials exists
-        """
+    def __init__(self, credentials_file):
+        """Store username and password for future use with a WikiClient.
 
-        if not user_name:
-            return
-
-        credentials_file = os.path.join(PATHS['global_config'], self.file_pattern.format(user_name.lower()))
+        Username and password are taken from the `credentials_file`,
+        which should be a JSON file with a "username" key and a "password" key.
+        """
         if not os.path.exists(credentials_file):
             raise InvalidUserFile(credentials_file, missing=True)
-        
+
         with open(credentials_file) as f:
             json_data = json.load(f)
         if json_data:
@@ -32,4 +24,3 @@ class AuthCredentials(object):
             self.username = json_data['username']
         else:
             raise InvalidUserFile(credentials_file)
-        
